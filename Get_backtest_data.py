f1acf50687a33c29a0a5e7643603249651638c59
Shoichiiriya <milkcookie0107@gmail.com>
2019-09-15 22:36:06 +0900

    backtestの確認コード追加、1分足テストデータ追加

--- Get_backtest_data.py
+++ Get_backtest_data.py
@@ -7,37 +7,7 @@ import json
 start_unix_time = 1483196400
 chart_sec = 60         # 保存したいローソク足の時間軸
 file = "./test_data_1min.json"   # 保存するファイル名
-start_period = "2018/05/07 00:00"
-end_period = "2018/05/08 00:00"
 
-
-# 価格ファイルからローソク足データを読み込む関数
-def get_price_from_file( min,path,start_period = None, end_period = None ):
-	file = open(path,"r",encoding="utf-8")
-	data = json.load(file)
-	
-	start_unix = 0
-	end_unix = 9999999999
-	
-	if start_period:
-		start_period = datetime.strptime(start_period,"%Y/%m/%d %H:%M")
-		start_unix = int(start_period.timestamp())
-	if end_period:
-		end_period = datetime.strptime( end_period,"%Y/%m/%d %H:%M")
-		end_unix = int(end_period.timestamp())
-	
-	price = []
-	for i in data["result"][str(min)]:
-		if i[0] >= start_unix and i[0] <= end_unix:
-			if i[1] != 0 and i[2] != 0 and i[3] != 0 and i[4] != 0:
-				price.append({ "close_time" : i[0],
-					"close_time_dt" : datetime.fromtimestamp(i[0]).strftime('%Y/%m/%d %H:%M'),
-					"open_price" : i[1],
-					"high_price" : i[2],
-					"low_price" : i[3],
-					"close_price": i[4] })
-	
-	return price
 
 def accumulate_data(min, path, before=0, after=0):
 	price = []
@@ -94,22 +64,11 @@ def accumulate_diff_data(min, path, before=0, after=0):
 
 # ---- ここからメイン ----
 # ここからメイン
-accumulate_data(chart_sec, file ,after=start_unix_time)
+#accumulate_data(chart_sec, file ,after=start_unix_time)
 # i = 0
-# add_unix = chart_sec * 6000 - 100
+add_unix = chart_sec * 6000 - 100
 # while i < 3:
 # 	add_unix = add_unix + (chart_sec * 6000 - 100)
 
 # 	# 差分の価格データを保存する
-# 	accumulate_diff_data(chart_sec, file, after=add_unix)
-
-
-
-# 	i = i + 1
-price = get_price_from_file(chart_sec, file)
-print("--------------------------")
-print("テスト期間：")
-print("開始時点 : " + str(price[0]["close_time_dt"]))
-print("終了時点 : " + str(price[-1]["close_time_dt"]))
-print(str(len(price)) + "件のローソク足データで検証")
-print("--------------------------")
+accumulate_diff_data(chart_sec, file, after=add_unix)
